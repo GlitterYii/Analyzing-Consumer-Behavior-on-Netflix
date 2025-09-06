5ffdf5218ecb9aa320d1a835d4428cf6b2a62b4d
GlitterYii <156504752+GlitterYii@users.noreply.github.com>
2025-09-06 16:18:27 +0700

    last edit

--- extract.py
+++ extract.py
@@ -3,10 +3,11 @@ import pandas as pd
 from datetime import datetime
 
 file_name = "netflix_titles.csv"
+desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+
 #extract file and store raw data to floder
-def extract(file_name):
+def extract(desktop_path,file_name):
     #find path and create floder at desktop
-    desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
     raw_dir = os.path.join(desktop_path, "data", "raw")
     os.makedirs(raw_dir, exist_ok=True)
 
@@ -23,40 +24,48 @@ def transform(df):
     #Cleaning data
 
     #Check data missing & duplicate
-    missing = df.isnull()
-    print(f"Data missing \n{missing.sum()}")
-    dup = df.duplicated()
-    print(f"Data duplicated    {dup.sum()}")
+    print(f"Check data missing \n{df.isnull().sum()}")
+    print(f"Check data duplicated = {df.duplicated().sum()}")
 
-    #check data type
-    print(df.dtypes)
+    #Drop duplicates
+    df.drop_duplicates(inplace=True)
+
+    #Check data type
+    print(f"Chaek data types \n{df.dtypes}")
 
     # Change dtype
     df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
     df["type"] = df["type"].astype("category")
+    df["release_yesr"] = df["release_year"].astype(int)
+    df["rating"] = df["rating"].astype("category")
 
-    # Process duration
+
+    # Process duration &  Drop original duration column correctly
     df['duration'] = df['duration'].astype(str)
-    df['duration_int'] = df['duration'].str.extract(r'(\d+)')[0].astype(float)
+    df['duration_int'] = df['duration'].str.extract(r'(\d+)')[0].astype('Int64')
     df['duration_unit'] = df['duration'].str.extract(r'([a-zA-Z ]+)')[0].str.strip()
-    df['duration_unit'] = df['duration_unit'].replace("Season", "Seasons")
-
-    # Drop original duration column correctly
+    df['duration_unit'] = df['duration_unit'].replace({'min': 'minutes', 'Season': 'seasons', 'Seasons': 'seasons'})
     df.drop(columns=['duration'], inplace=True) 
+    
+    # Split into list
+    for i in ['cast', 'listed_in', 'director']:
+        df[i] = df[i].str.split(', ')
 
-    # Split cast into list
-    df['cast'] = df['cast'].str.split(', ')
-
+    print(f"Data types \n{df.dtypes}")
     print(df.head())
 
     return df
 
-def load(df,filename):
-    df.to_csv(filename)
+def load(desktop_path,df,filename):
+    ETL_dir = os.path.join(desktop_path,"data","ETL") 
+    os.makedirs(ETL_dir, exist_ok=True)
+
+    file_path = os.path.join(ETL_dir,f"ETL_{filename}")
+    df.to_csv(file_path)
 
 def pipeline(file_name):
-    extract_data = extract(file_name)
+    extract_data = extract(desktop_path,file_name)
     transform_data = transform(extract_data)
-    load_data = load(transform_data,f"{file_name}_ETL.csv")
+    load_data = load(desktop_path,transform_data,file_name)
 
 pipeline(file_name)
