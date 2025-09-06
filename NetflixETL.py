b198216e2898c22084c88d2665a636ac4503b1cd
GlitterYii <156504752+GlitterYii@users.noreply.github.com>
2025-09-06 17:07:43 +0700

    last edit

--- NetflixETL.py
+++ NetflixETL.py
@@ -21,28 +21,44 @@ def extract(desktop_path,file_name):
     return extracted_data
 
 def transform(df):
-    #Cleaning data
+    # Cleaning data
 
-    #Check data missing & duplicate
+    # Check for duplicates first, before modifying data types
     print(f"Check data missing \n{df.isnull().sum()}")
-    print(f"Check data duplicated = {df.duplicated().sum()}")
+    print(f"Check data duplicated = {df.duplicated().sum()}\n")
 
-    #Drop duplicates
+    # Drop duplicates
     df.drop_duplicates(inplace=True)
+    
+    # Handle missing values in specific columns
+    df['rating'] = df['rating'].fillna("Not Rated")
 
-    #Check data type
-    print(f"Chaek data types \n{df.dtypes}")
+    # Fill 'director', 'cast', 'country' with 'NaN'
+    fill_columns_str = ['director', 'cast', 'country']
+    for col in fill_columns_str:
+        df[col] = df[col].fillna('NaN')
+    
+    # Check data type
+    print(f"Chaek data types \n{df.dtypes}\n")
 
-    # Change dtype
+    # Change dtype and handle missing values for 'date_added'
     df["date_added"] = pd.to_datetime(df["date_added"], errors="coerce")
+    date_added_mode = df["date_added"].mode()[0]
+    df["date_added"] = df["date_added"].fillna(date_added_mode)
+
+    # Change dtype and handle missing values for 'release_year'
+    df["release_year"] = df["release_year"].astype('Int64')
+    release_year_mode = df["release_year"].mode()[0]
+    df["release_year"] = df["release_year"].fillna(release_year_mode)
+
     df["type"] = df["type"].astype("category")
-    df["release_yesr"] = df["release_year"].astype(int)
     df["rating"] = df["rating"].astype("category")
 
-
-    # Process duration &  Drop original duration column correctly
-    df['duration'] = df['duration'].astype(str)
+    # Process duration & Drop original duration column correctly
+    df['duration'] = df['duration'].fillna('0 min')
     df['duration_int'] = df['duration'].str.extract(r'(\d+)')[0].astype('Int64')
+    df['duration_int'] = df['duration_int'].fillna(df["duration_int"].mean())
+    
     df['duration_unit'] = df['duration'].str.extract(r'([a-zA-Z ]+)')[0].str.strip()
     df['duration_unit'] = df['duration_unit'].replace({'min': 'minutes', 'Season': 'seasons', 'Seasons': 'seasons'})
     df.drop(columns=['duration'], inplace=True) 
@@ -51,7 +67,8 @@ def transform(df):
     for i in ['cast', 'listed_in', 'director']:
         df[i] = df[i].str.split(', ')
 
-    print(f"Data types \n{df.dtypes}")
+    print(f"Check data missing \n{df.isnull().sum()}")
+    print(f"Data types \n{df.dtypes}\n")
     print(df.head())
 
     return df
